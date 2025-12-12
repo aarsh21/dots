0efee1ee1eca367893961cc970e6fcd430e2d5bf
aarsh21 <aarshpersonal@proton.me>
2025-12-12 21:59:24 +0530

    2025 changes

--- hypr/scripts/recording_status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-if pgrep -x "wf-recorder" > /dev/null; then
-  echo '{"text": "🔴 REC", "tooltip": "Recording active", "class": "recording-active"}'
-else
-  echo '{"text": "", "tooltip": "", "class": "recording-inactive"}'
-fi
